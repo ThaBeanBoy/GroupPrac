3cd1ed42dd454f1b4d7bc00544bea07c8e4c74f9
Sbuda45 <91832693+Sbuda45@users.noreply.github.com>
2021-10-04 18:30:12 +0200

    Add files via upload

---
+++ GeneticType.vb
@@ -0,0 +1,50 @@
+﻿' ***************************************************************** 
+' Team Number: 20
+' Team Member 1 Details: Chipoyera, TG (220150124) 
+' Team Member 2 Details: Hlebela, JM (221026973) 
+' Team Member 3 Details: NDHLOVU, AJ(221020749) 
+' Team Member 4 Details: Ntuli, SD (221076674) 
+' Practical: Team Project 
+' Class name: (name of the class) 
+' *****************************************************************
+
+Option Strict On
+Option Explicit On
+Option Infer On
+
+Public Class GeneticType
+
+    Private _Name As String
+    Private _Inherited As Boolean
+
+    Private Sub New(Name As String, Inherited As Boolean)
+        _Name = Name
+        _Inherited = Inherited
+    End Sub
+
+    Public ReadOnly Property Name As String
+        Get
+            Return _Name
+        End Get
+    End Property
+
+    Public Property Inherited As Boolean
+        Get
+            Return _Inherited
+        End Get
+        Set(value As Boolean)
+            _Inherited = value
+        End Set
+    End Property
+
+    Public Function FindLifeSpan() As Double
+        Dim temp As Double
+        If _Inherited = True Then
+            temp = 40.5
+        Else
+            temp = 50.7
+        End If
+        Return temp
+    End Function
+
+End Class
